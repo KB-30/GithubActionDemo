34d15f519b2dfb6821a3de4b58f61ba0ef1a9b4e
KB-30 <148705294+KB-30@users.noreply.github.com>
2024-01-09 13:47:18 +0530

    Update Kmain.tf

--- Infra_creation/Kmain.tf
+++ Infra_creation/Kmain.tf
@@ -1,9 +1,9 @@
 terraform {
     required_version = "0.14.8"
     backend "azurerm" {
-        resource_group_name = "kanch-rg1"
-        storage_account_name = "kanchstorageacc"
-        container_name = "tfstatefile"
+        resource_group_name = "KanchGit"
+        storage_account_name = "kanchstorage12"
+        container_name = "kanchcontainer"
         key = "terraform.tfstate"
         arm_subscription_id = "793729a4-a745-4e4f-8b8d-88bda288a86a"
         arm_tenant_id = "1e9cc706-c3fd-4b8c-9dbd-a073e7384b56"
